6407a5cf7fb7f1df54594703ecb3230d9ad1a2e1
anwararid <enverelarit290@gmail.com>
2026-08-08 01:21:12 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhvLb2QMMe7dy-vQ_3bwKtZK5XXy8lbWM",
+  authDomain: "ayder-sofra.firebaseapp.com",
+  projectId: "ayder-sofra",
+  storageBucket: "ayder-sofra.firebasestorage.app",
+  messagingSenderId: "459245770853",
+  appId: "1:459245770853:web:8d32163fef443a2b7c8e8c",
+  measurementId: "G-5TWCFGPXYR"
+};
