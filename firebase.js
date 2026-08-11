710639b9ec16678076095fed057cba0ccef39ec9
anwararid <enverelarit290@gmail.com>
2026-08-11 12:05:19 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,14 +11,10 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { firebaseConfig } from "./firebase-config.js";
 
-// 1. تهيئة تطبيق Firebase
 const app = initializeApp(firebaseConfig);
-
-// 2. تهيئة خدمات Auth و Firestore
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// 3. تصدير الأدوات لاستخدامها في باقي الملفات
 export {
   auth,
   db,
